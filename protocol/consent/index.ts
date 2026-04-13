debb47ebaf25e27a5882e2ad0a63cff24b2337ff
vicvalch <36937201+vicvalch@users.noreply.github.com>
2026-04-13 10:42:47 -0600

    Refine consent time semantics and evaluation order (#115)

--- protocol/consent/index.ts
+++ protocol/consent/index.ts
@@ -3,6 +3,9 @@ export { normalizeConsent } from './consent-normalizer';
 export { validateConsent } from './consent-validator';
 export { evaluateConsentState, isConsentActive, isConsentRevoked } from './consent-state';
 export { doesConsentAllowScope } from './consent-machine';
+export { evaluateConsent } from './evaluate';
+export { isValidConsentRecord, isValidConsentRequest } from './validate';
+export { CONSENT_DECISION_REASON_CODES } from './types';
 
 export { ConsentParseError, ConsentValidationError } from './consent-errors';
 
@@ -14,3 +17,10 @@ export type {
   ConsentEvaluationOptions,
   ConsentScopeRequest,
 } from './consent-types';
+
+export type {
+  ConsentRecord,
+  ConsentRequest,
+  ConsentDecision,
+  ConsentDecisionReasonCode,
+} from './types';
