9e2ec099a5d1405fcc65ee3001cd70bc6e6cb6fa
Victor Valverde <vicvalch@gmail.com>
2026-05-13 23:11:51 -0600

    refactor: remove enterprise orchestration runtimes from core protocol

--- runtime/runtime-negotiation/negotiation-proposal.ts
+++
@@ -1,8 +0,0 @@
-import { NegotiationProposal } from './types';
-
-export function submitNegotiationProposal(proposal: NegotiationProposal): NegotiationProposal {
-  if (!proposal.explainabilityRefs.length) {
-    throw new Error('Negotiation proposal requires explainability refs');
-  }
-  return proposal;
-}
